45a17fd2dc82defe44af5cbbf377d74df52403fd
Adam Hintz <adam000@users.noreply.github.com>
2015-06-10 19:00:57 -0700

    Hello world and some basic stuff
    Just messing around...

---
+++ src/hello_world.c
@@ -0,0 +1,41 @@
+#include <pebble.h>
+
+Window *window;
+TextLayer *text_layer;
+   
+void handle_init(void) {
+	// Create a window and text layer
+	window = window_create();
+	text_layer = text_layer_create(GRect(0, 50, 144, 30));
+	
+	// Set the text, font, and text alignment
+	text_layer_set_text(text_layer, "Hi, I'm a Pebble!");
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+   
+   text_layer_set_background_color(text_layer, COLOR_FALLBACK(GColorCyan, GColorBlack));
+	text_layer_set_text_color(text_layer, COLOR_FALLBACK(GColorBlueMoon, GColorWhite));
+   
+	// Add the text layer to the window
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+
+	// Push the window
+	window_stack_push(window, true);
+	
+	// App Logging!
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
+}
+
+void handle_deinit(void) {
+	// Destroy the text layer
+	text_layer_destroy(text_layer);
+	
+	// Destroy the window
+	window_destroy(window);
+}
+
+int main(void) {
+	handle_init();
+	app_event_loop();
+	handle_deinit();
+}
